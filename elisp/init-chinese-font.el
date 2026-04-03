a018181be02748e1853bf5975f6da5e482b22525
Chang-Hao Fang <fangch@stu.scu.edu.cn>
2026-04-03 22:50:10 +0800

    添加 WSL/Linux 中文字体

--- elisp/init-chinese-font.el
+++ elisp/init-chinese-font.el
@@ -35,21 +35,22 @@
 ;;
 ;;; Code:
 
-(when (eq system-type 'darwin)
+(when *sys/mac*
   (setq fonts '("SF Mono" "冬青黑体简体中文"))
   (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 14)))
 
-(when (eq system-type 'windows-nt)
+(when *sys/win32*
   (setq fonts '("Consolas" "微软雅黑"))
   (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'prepend)
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 14)))
 
-(when (eq system-type 'gnu/linux)
-  (setq fonts '("SF Mono" "Noto Sans Mono CJK SC"))
+(when (or *sys/wsl* *sys/linux*)
+  (setq fonts '("Ubuntu Mono" "Monospace"))
   (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
+  (set-fontset-font t 'han "Noto Sans CJK SC" nil 'prepend)
   (set-face-attribute 'default nil :font
                       (format "%s:pixelsize=%d" (car fonts) 14)))
 
